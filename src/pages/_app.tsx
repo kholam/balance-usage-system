b0203a05940442e4aea2d9389aa154bed47392b3
khaliddraig <khaliddraig1992@gmail.com>
2022-08-30 21:42:27 +0800

    minimal set up of react intl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import type { AppProps } from 'next/app'
-import {GlobalStyles} from "../components";
+import { GlobalStyles } from "../components";
+import { IntlProvider } from 'react-intl'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <React.Fragment>
-      <GlobalStyles/>
-      <Component {...pageProps} />
-    </React.Fragment>
+      <IntlProvider locale="en" defaultLocale="en">
+        <React.Fragment>
+          <GlobalStyles/>
+          <Component {...pageProps} />
+        </React.Fragment>
+      </IntlProvider>
   )
 }
 
