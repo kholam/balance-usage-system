069fa3976899dc7b2768a6b5e7c7c4b02b6f77bd
khaliddraig <khaliddraig1992@gmail.com>
2022-08-30 10:47:00 +0800

    project color scheme

---
+++ src/assets/styles/colors.ts
@@ -0,0 +1,16 @@
+
+const COLORS = {
+    SNOW: '#F9FAFB',
+    WHITE: "#FFFFFF",
+    SIGNAL_GREY: "#999999",
+    DIM_GRAY: "#666666",
+    BLACK: "#000000",
+    LAVENDER_INDIGO: '#835FEE', // color primary
+    BRIGHT_LAVENDER: '#a38cf3', // color secondary
+    PLATINUM: '#e5e7eb',
+    MAGNOLIA: '#f5f3fe',
+    WHITE_SMOKE: '#f7f7f8',
+}
+
+
+export default COLORS;
