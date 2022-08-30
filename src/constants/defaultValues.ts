bbf40611fc7f026ad93b6ba3a4d4eea09493c07a
khaliddraig <khaliddraig1992@gmail.com>
2022-08-30 22:13:05 +0800

    display users in dashboard page

---
+++ src/constants/defaultValues.ts
@@ -0,0 +1,3 @@
+
+
+export const TOTAL_CREDITS = 50;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,55 @@ import React from "react";
 import type { NextPage } from 'next';
 import { getUsers } from "../lib/airtable";
 import { User } from "../interface";
+import Head from "next/head";
+import { Card, Container } from "../components";
+import { CardBody, CardTitle } from "../components/card";
+import tw from "twin.macro";
+import { TableHeader } from "../components";
+import UserListItem from "../components/common/UserListItem";
 
 interface HomeProps {
     users: User[];
 }
 
-const Home: NextPage<HomeProps> = ({ users}) => {
-    console.log('users', users);
 
+
+const Home: NextPage<HomeProps> = ({ users}) => {
 
   return (
     <React.Fragment>
+        <Head>
+            <title>Dashboard</title>
+        </Head>
+        <Container>
+            <Card>
+                <CardTitle>Users</CardTitle>
+                <CardBody>
+                    <table tw="w-full text-left table-auto">
+                        <thead>
+                        <tr>
+                            <TableHeader>Avatar</TableHeader>
+                            <TableHeader>Name</TableHeader>
+                            <TableHeader>Username</TableHeader>
+                            <TableHeader>Credits</TableHeader>
+                            <TableHeader>Date Joined</TableHeader>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            users.map((user: User, index)=>{
+                                return (
+                                    <UserListItem user={user} key={index}/>
+                                )
+
+                            })
+                        }
+
+                        </tbody>
+                    </table>
+                </CardBody>
+            </Card>
+        </Container>
     </React.Fragment>
   )
 }
@@ -21,7 +59,7 @@ export async function getStaticProps() {
     const users: User[] = await getUsers() || [];
 
     return {
-        /* page the data to the component as props */
+        /* pass the data as props to Home component */
         props: {
             users: users,
         },
