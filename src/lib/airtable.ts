609e64e58f5c4cf4c710ad817e78d3182c478606
khaliddraig <khaliddraig1992@gmail.com>
2022-08-30 19:16:27 +0800

    returns user records from air table

--- src/lib/airtable.ts
+++ src/lib/airtable.ts
@@ -1,5 +1,6 @@
 import Airtable from "airtable";
-
+import {User} from "../interface";
+import { getMinifiedRecords } from "../utils/helpers";
 // authenticate and configure Airtable API
 Airtable.configure({
     apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY
@@ -11,4 +12,17 @@ const base = Airtable.base(`${process.env.NEXT_PUBLIC_AIRTABLE_BASE_KEY}`);
 // reference table created on Airtable
 const table = base(`${process.env.NEXT_PUBLIC_AIRTABLE_TABLE_NAME}`);
 
-export { table }
+
+/* returns ony first page of records in the database
+* Page size is 100 by default
+* */
+const getUsers = async (): Promise<User[]> => {
+
+    const userRecords = await table
+            .select()
+            .firstPage();
+
+    return getMinifiedRecords(userRecords) as User[];
+}
+
+export { table, base, getUsers }
