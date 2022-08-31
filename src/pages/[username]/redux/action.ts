581cded1a0a180088c5696a22548f041ed9dfacf
khaliddraig <khaliddraig1992@gmail.com>
2022-08-31 13:50:38 +0800

    action types for the reducer

---
+++ src/pages/[username]/redux/action.ts
@@ -0,0 +1,9 @@
+// actions for the reducer
+import {Credits} from "../../../interface";
+import { UpdateBalanceActionType } from "./state";
+
+// actions for the reducer
+export type UpdateBalanceAction = {
+    type: UpdateBalanceActionType;
+    payload: Credits;
+}
